6ea69704178c7ae4dddf054ac319796700d210fa
Elenilson15 <112187042+Elenilson15@users.noreply.github.com>
2022-08-26 12:03:50 -0300

    Add files via upload

---
+++ js/js-c9b76c162ea3ae4dfc7563c969dfbc0c.js
@@ -0,0 +1,4 @@
+function twentytwentyoneResponsiveEmbeds(){var proportion,parentWidth;document.querySelectorAll('iframe').forEach(function(iframe){if(iframe.width&&iframe.height){proportion=parseFloat(iframe.width)/parseFloat(iframe.height);parentWidth=parseFloat(window.getComputedStyle(iframe.parentElement,null).width.replace('px',''));iframe.style.maxWidth='100%';iframe.style.maxHeight=Math.round(parentWidth/proportion).toString()+'px'}})}
+twentytwentyoneResponsiveEmbeds();window.onresize=twentytwentyoneResponsiveEmbeds
+;
+/*This file was exported by "Export WP Page to Static HTML" plugin which created by ReCorp (https://myrecorp.com) */
